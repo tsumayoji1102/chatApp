777cd79001274158c1749913616576ff3fc19bcd
塩見陵介 <ryosukesukairain1@gmail.com>
2020-06-11 20:24:20 +0900

    いろいろ掴むための練習をしてみた（基本実装、テストなど）

--- chatapp/lib/main.dart
+++ chatapp/lib/main.dart
@@ -4,35 +4,36 @@ void main() {
   runApp(MyApp());
 }
 
+// 静的な部分を構築
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  final title   = "サンプルのタイトル";
+  final product = "メッセージはここ";
   @override
   Widget build(BuildContext context) {
+    //　マテリアルアプリを返す（クパチーノにもできる？）
     return MaterialApp(
-      title: 'Flutter Demo',
+      // タイトルを設定
+      title: 'tsumayoji App',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
+
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // homeArea(Scaffoldで、ベーシックなマテリアルデザインにできる）
+      home: MyHomePage(
+        title: this.title,
+        product: this.product,
+      )
     );
   }
 }
 
+// ウィジェットクラス
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+
+  MyHomePage({Key key, this.title, this.product}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -44,24 +45,34 @@ class MyHomePage extends StatefulWidget {
   // always marked "final".
 
   final String title;
+  final String product;
 
+  // ここで、ステートを読み込む
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+// ステートクラス（ウィジェットで読み込ませる）
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Product _product;
+
+  static final _products = [Product("りんご", 200), Product("みかん", 300)];
+  // 表示するもの
+
+  @override
+  void initState(){
+    super.initState();
+    _product = _products.first;
   }
+
+  void _setProduct(){
+    setState(() {
+      // シャッフル
+      _product = (_products..shuffle()).first;
+    });
+
+}
 
   @override
   Widget build(BuildContext context) {
@@ -77,41 +88,29 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
+      body: Text(_product.toString(), style: TextStyle(fontSize: 32.0),),
+      // 右下にボタンが出せる
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _setProduct,  // タップした時
+        tooltip: 'set product.', //
+        child: Icon(Icons.star), //
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+// オブジェクトを定義
+class Product{
+  String _fruit;
+  int    _price;
+
+  // コンストラクタ
+  Product(this._fruit, this._price) :super();
+
+  // 商品、価格
+  @override
+  String toString(){
+    return this._fruit + this._price.toString() + "Yen";
+  }
+}
+
--- chatapp/test/widget_test.dart
+++ chatapp/test/widget_test.dart
@@ -12,19 +12,16 @@ import 'package:chatapp/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    // テストの実行
+    await tester.pumpWidget(MyApp()); // appのインスタンス作成
+    await tester.pump();              // フレームへのトリガー(launch)
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    /*
+    extept(x1 , x2)
+     */
 
     // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    //expect(find.text('0'), findsNothing);
+    //expect(find.text('1'), findsOneWidget);
   });
 }
